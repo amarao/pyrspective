e991453d1651143ac98ef0d9742c4013df15ea9c
George Shuklin <george.shuklin@gmail.com>
2014-09-22 05:21:44 +0300

    Add opaqueness for heler line, fix bug with right line clipping

--- 1perspective.py
+++ 1perspective.py
@@ -23,7 +23,7 @@ def get_step(lower, upper, varnish, max_x, step_size,step):
     elif projected_X > max_x:
         a = projected_X - max_x
         L = lower - upper
-        M = varnish
+        M = max_x - varnish  # triangle in other direction
         c = float (L * a) / (M + a)
         y = lower - c
         return max_x,y
@@ -36,11 +36,12 @@ class Canvas:
         self.y = 1080
         self.dotsize = 3
         self.horizon = 100
-        self.varnish = self.x/2 
+        self.varnish = self.x/3
         self.horizon_color = svgwrite.rgb(50, 50, 50, '%')
         self.helper_color = svgwrite.rgb(50, 50, 75, '%')
+        self.helper_opacity = 0.2
         self.filename='1perspective.svg'
-        self.bottom = self.y - self.horizon
+        self.bottom = int( self.y * 0.9)
         self.step_size = 64
         
 
@@ -48,13 +49,17 @@ c = Canvas()
 
 dwg = svgwrite.Drawing(c.filename, size=(c.x,c.y), profile='tiny')
 dwg.add(dwg.line((0, c.horizon), (c.x, c.horizon), stroke = c.horizon_color))
-dwg.add(dwg.circle(center = (c.varnish, c.horizon), r = c.dotsize , stroke = c.horizon_color))
+dwg.add(dwg.circle(center = (c.varnish, c.horizon), r = c.dotsize , stroke = c.horizon_color, stroke_opacity = c.helper_opacity))
 dwg.add(dwg.line((0, c.bottom), (c.x, c.bottom), stroke = c.helper_color))
-for step in range(-128, 129):
+for step in range(-64,129):  # TODO calculate for symmetry
     x, y = get_step(c.bottom, c.horizon, c.varnish, c.x, c.step_size, step)
-    dwg.add(dwg.line((x, y), (c.varnish, c.horizon), stroke = c.helper_color))
+    if x == c.varnish:  # vertical line, double opacity if possible
+        opacity = min (1, c.helper_opacity * 2)
+    else:
+        opacity = c.helper_opacity
+    dwg.add(dwg.line((x, y), (c.varnish, c.horizon), stroke = c.helper_color, stroke_opacity=opacity))
     if y == c.bottom:
-        dwg.add(dwg.circle(center = (x, c.bottom), r = c.dotsize, stroke = c.helper_color))
+        dwg.add(dwg.circle(center = (x, c.bottom), r = c.dotsize, stroke = c.helper_color, stroke_opacity = c.helper_opacity))
 
 
 dwg.save()
